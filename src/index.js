b04d946548d81a04c127b2192833d7a117571216
Vlad <goncharov-vlad@ukr.net>
2019-05-09 14:04:00 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+
+/*import './js/common'*/
+import './css/main.css'
+import './scss/main.scss'
+import SmoothScroll from './js/smooth-scroll.min'
+
+var scroll = new SmoothScroll('a[href*="#"]');
+// import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+
+// import 'vue'
+// import Vue from 'vue'
+window.Vue = require('vue')
+
