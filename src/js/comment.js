f856d9f2a2c8b6943064573f7aa5dbe87ae8399f
mygithubaccount3 <43136479+mygithubaccount3@users.noreply.github.com>
2019-05-13 00:06:42 +0300

    responsive fixes

--- src/js/comment.js
+++ src/js/comment.js
@@ -1,3 +1,5 @@
+import Vue from 'vue'
+
 ;(($) => {
     $('.btn').click((e) => {
         e.preventDefault();
@@ -25,21 +27,22 @@
     $(document).on('click', '.item-edit', function (e) {
         e.preventDefault();
         if($(e.target).next().attr('contenteditable')) {
-            $(e.target).next().attr('contenteditable', false).css('border', '1px solid #60e3a1');
-            $(e.target).next().next().attr('contenteditable', false).css('border', '1px solid #60e3a1');
+            $(e.target).next().attr('contenteditable', false).css('border', 'none');
+            $(e.target).next().next().attr('contenteditable', false).css('border', 'none');
         }
         else {
-            $(e.target).next().attr('contenteditable', true).css('border', 'none');
-            $(e.target).next().next().attr('contenteditable', true).css('border', 'none');
+            $(e.target).next().attr('contenteditable', true).css('border', '1px solid #60e3a1', 'box-shadow', 'inset 3px 3px 28px 0px rgba(12,242,66,1)');
+            $(e.target).next().next().attr('contenteditable', true).css('border', '1px solid #60e3a1', 'box-shadow', 'inset 3px 3px 28px 0px rgba(12,242,66,1)');
         }
 
     });
 
     $(document).on('click', '.item-link', function (e) {
         if (typeof(Storage) !== "undefined") {
-            sessionStorage.title = $(e.target).prev().prev().val();
-            sessionStorage.comment = $(e.target).prev().val();
+            sessionStorage.title = $(e.target).prev().prev().text();
+            sessionStorage.comment = $(e.target).prev().text();
         }
+        else $('.comment-title').append('Your browser does not support storage')
     });
 
     let app = new Vue({
