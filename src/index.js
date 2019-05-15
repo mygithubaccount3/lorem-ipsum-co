552b474a4c0924321f89b17e37e94fe5a8529599
mygithubaccount3 <43136479+mygithubaccount3@users.noreply.github.com>
2019-05-15 15:18:20 +0300

    add hover effects

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import './css/main.css'
 import './scss/main.scss'
 import './css/overline-from-center.css'
+import './css/hollow.css'
+import './css/icon-back.css'
+import './css/glow.css'
+import './css/bob.css'
 import './js/comment'
 import SmoothScroll from './js/smooth-scroll.min'
 
