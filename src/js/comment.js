0c02065b65c934ef7a8e664cc605162922df395e
mygithubaccount3 <43136479+mygithubaccount3@users.noreply.github.com>
2019-05-17 11:12:57 +0300

    move comment's buttons to comment's individual page

--- src/js/comment.js
+++ src/js/comment.js
@@ -1,27 +1,45 @@
 ;(($) => {
-    $(document).on('click', '.item-close', function (e) {
+    $(document).on('click', '.comment-delete', function (e) {
         e.preventDefault();
         $(e.target).closest('.grid-item').css('display', 'none')
     });
 
-    $(document).on('click', '.item-edit', function (e) {
+    $(document).on('click', '.comment-edit', function (e) {
         e.preventDefault();
-        $(e.target).nextAll('p').attr('contenteditable', true).css('box-shadow', 'inset 0px 0px 5px 0px #60e3a1');
+        $(e.target).nextAll('.comment-title, .comment-text').attr('contenteditable', true).css('box-shadow', 'inset 0px 0px 5px 0px #60e3a1');
         $(e.target).attr('disabled', true);
         $(e.target).next().removeAttr('disabled');
     });
 
-    $(document).on('click', '.item-save', function (e) {
+    $(document).on('click', '.comment-save', function (e) {
         e.preventDefault();
-        $(e.target).nextAll('p').removeAttr('contenteditable').css('box-shadow', 'none');
-        $(e.target).attr('disabled', true);
-        $(e.target).prev().removeAttr('disabled');
+        $.ajax("https://5cbef81d06a6810014c66193.mockapi.io/api/comments/" + sessionStorage.id,
+            {
+                "method": "PUT",
+                "timeout": 0,
+                "headers": {
+                    "Content-Type": "application/json"
+                },
+                "data": "{\n    \"created_at\": " + Date.now() + ",\n    \"title\": " + "\"" + $(e.target).nextAll('.comment-title').text() + "\"" + ",\n    \"body\": " + "\"" + $(e.target).nextAll('.comment-text').text() + "\"" + "\n}",
+                "beforeSend": function () {
+                    $(e.target).nextAll('.comment-title, .comment-text').removeAttr('contenteditable').css('box-shadow', 'none');
+                    $('<div class="comments-loader" style="position: absolute; top: 0; right: 0; bottom: 0; left: 0; background-color: white; opacity: 0.5"><i class="fa fa-cog" aria-hidden="true"></i></div>').insertAfter('.comment-save')
+                },
+                "complete": function () {
+                    $('.comments-loader').remove();
+                    $(e.target).attr('disabled', true);
+                    $(e.target).prev().removeAttr('disabled');
+                }
+            }).done(function () {
+
+        });
     });
 
     $(document).on('click', '.item-link', function (e) {
         if (typeof(Storage) !== "undefined") {
             sessionStorage.title = $(e.target).prev().prev().text();
             sessionStorage.comment = $(e.target).prev().text();
+            sessionStorage.id = $(e.target).next().val();
         }
         else $('.comment-title').append('Your browser does not support storage')
     });
