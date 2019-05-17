4ceb424226da21162639c9d479681206c1c70116
mygithubaccount3 <43136479+mygithubaccount3@users.noreply.github.com>
2019-05-17 23:11:46 +0300

    make comment deletion possible, centered elements on mobile

--- src/js/comment.js
+++ src/js/comment.js
@@ -1,7 +1,25 @@
 ;(($) => {
     $(document).on('click', '.comment-delete', function (e) {
         e.preventDefault();
-        $(e.target).closest('.grid-item').css('display', 'none')
+        $.ajax("https://5cbef81d06a6810014c66193.mockapi.io/api/comments/" + sessionStorage.id,
+            {
+                "method": "DELETE",
+                "timeout": 0,
+                "beforeSend": function () {
+                    $('<div class="comments-loader" style="position: absolute; top: 0; right: 0; bottom: 0; left: 0;' +
+                        'background-color: white; opacity: 0.8; padding-top: 100px">' +
+                        '<i class="fa fa-cog" aria-hidden="true"></i></div>').insertAfter('.comment-save')
+                },
+                "complete": function () {
+                    $('.comments-loader').replaceWith('<div class="comments-loader" style="position: absolute; top: 0; ' +
+                        'right: 0; bottom: 0; left: 0; background-color: white; opacity: 0.8; padding-top: 100px">' +
+                        '<i class="fa fa-check" aria-hidden="true" style="animation: none; color: #60e3a1"></i></div>');
+                }
+            }).done(function () {
+                setTimeout(() => {
+                    window.location.href = 'all-comments.html'
+                }, 3000);
+        });
     });
 
     $(document).on('click', '.comment-edit', function (e) {
@@ -20,19 +38,23 @@
                 "headers": {
                     "Content-Type": "application/json"
                 },
-                "data": "{\n    \"created_at\": " + Date.now() + ",\n    \"title\": " + "\"" + $(e.target).nextAll('.comment-title').text() + "\"" + ",\n    \"body\": " + "\"" + $(e.target).nextAll('.comment-text').text() + "\"" + "\n}",
+                "data": "{\n    \"created_at\": " + Date.now() + ",\n    \"title\": " + "\"" +
+                         $(e.target).nextAll('.comment-title').text() + "\"" + ",\n    \"body\": " + "\"" +
+                         $(e.target).nextAll('.comment-text').text() + "\"" + "\n}",
                 "beforeSend": function () {
                     $(e.target).nextAll('.comment-title, .comment-text').removeAttr('contenteditable').css('box-shadow', 'none');
-                    $('<div class="comments-loader" style="position: absolute; top: 0; right: 0; bottom: 0; left: 0; background-color: white; opacity: 0.5"><i class="fa fa-cog" aria-hidden="true"></i></div>').insertAfter('.comment-save')
+                    $('<div class="comments-loader" style="position: absolute; top: 0; right: 0; bottom: 0; left: 0;' +
+                                                          'background-color: white; opacity: 0.8; padding-top: 100px">' +
+                                                          '<i class="fa fa-cog" aria-hidden="true"></i></div>').insertAfter('.comment-save')
                 },
                 "complete": function () {
                     $('.comments-loader').remove();
                     $(e.target).attr('disabled', true);
                     $(e.target).prev().removeAttr('disabled');
+                    sessionStorage.title = $(e.target).next().text();
+                    sessionStorage.comment = $(e.target).nextAll('.comment-text').text();
                 }
-            }).done(function () {
-
-        });
+            })
     });
 
     $(document).on('click', '.item-link', function (e) {
